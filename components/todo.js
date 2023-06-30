039e48cd2e426b88214e18b333cf786d9f17591e
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-06-30 12:26:14 +0200

    To do list style changes

--- components/todo.js
+++ components/todo.js
@@ -1,20 +1,23 @@
 "use client"
 
 import { useState } from "react";
-
-
+import { AiFillDelete } from "react-icons/ai";
 
 
 function TDEntry({ text, checked, handleCheck, handleRemove }) {
     return (
-        <tr>
-            <td>
+        <tr className="flex gap-4 border-b py-2">
+            <td className="grow text-xl my_text_anim transition-all ease-linear">
                 {checked ?
-                    <span className="line-through">{text}</span> : text
+                    <span className="line-through text-gray-500">{text}</span> : text
                 }
             </td>
-            <td><input type="checkbox" checked={checked} onChange={handleCheck} /></td>
-            <td><button onClick={handleRemove}>DEL</button></td>
+            <td className="">
+                <label className="">
+                    <input className="w-6 h-6 transition-all ease-linear hover:ring-4 ring-highlight" type="checkbox" checked={checked} onChange={handleCheck} />
+                </label>
+            </td>
+            <td><button onClick={handleRemove}><AiFillDelete className="text-2xl ease-linear transition-colors hover:text-red-600"/></button></td>
         </tr>
     );
 }
@@ -68,12 +71,12 @@ export default function ToDoList() {
     })
 
     return (
-        <div className="flex-col grow p-5 bg-primary border border-white shadow-lg shadow-highlight_darker 
-        min-h-[40rem] justify-center">
-            <p className="text-3xl p-3 text-center">TO DO LIST</p>
-            <div className="container mx-6">
+        <div className="flex flex-col gap-2 p-5 bg-primary border border-white shadow-lg shadow-highlight_darker 
+        min-h-[40rem] justify-start">
+            <p className="basis-2 text-3xl p-3 text-center">TO DO LIST</p>
+            <div className="">
                 <form method="post"
-                    className=""
+                    className="flex p-2 gap-2"
                     onSubmit={e => {
                         e.preventDefault();
                         addEntry();
@@ -83,24 +86,15 @@ export default function ToDoList() {
                         value={entryField}
                         placeholder="type here"
                         onChange={e => setEntryField(e.target.value)}
-                        className="bg-primary my_hover_anim" />
-                    <button type="submit" className="my_hover_anim p-1">Add</button>
+                        className="grow text-xl bg-primary hover:border-highlight_darker hover:text-blue-400 my_text_anim border p-2 " />
+                    <button type="submit" className="text-xl my_text_anim my_button p-1">Add</button>
                 </form>
             </div>
-            <div>
-                <table className="min-w-[20rem]">
-                    <thead>
-                        <tr>
-                            <th>Objective</th>
-                            <th>CHECK</th>
-                            <th>DEL</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {rows}
-                    </tbody>
-                </table>
-            </div>
+            <table className="table-auto border-collapse min-w-[20rem]">
+                <tbody className="">
+                    {rows}
+                </tbody>
+            </table>
         </div>
     )
 }
