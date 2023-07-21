8e1d0424069aad19bdce41511155dacb115089e5
Michal Krulich <115186096+MichaelTheSynthCat@users.noreply.github.com>
2023-07-21 18:02:04 +0200

    ToDoList bug fix

--- components/todo.js
+++ components/todo.js
@@ -1,40 +1,52 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import { AiFillDelete } from "react-icons/ai";
-
 
 function TDEntry({ text, checked, handleCheck, handleRemove }) {
     return (
         <tr className="flex gap-4 border-b py-2">
             <td className="grow text-xl my_text_anim transition-all ease-linear">
-                {checked ?
-                    <span className="line-through text-gray-500">{text}</span> : text
-                }
+                {checked ? (
+                    <span className="line-through text-gray-500">{text}</span>
+                ) : (
+                    text
+                )}
             </td>
             <td className="">
                 <label className="">
-                    <input className="w-6 h-6 transition-all ease-linear hover:ring-4 ring-highlight" type="checkbox" checked={checked} onChange={handleCheck} />
+                    <input
+                        className="w-6 h-6 transition-all ease-linear hover:ring-4 ring-highlight"
+                        type="checkbox"
+                        checked={checked}
+                        onChange={handleCheck}
+                    />
                 </label>
             </td>
-            <td><button onClick={handleRemove}><AiFillDelete className="text-2xl ease-linear transition-colors hover:text-red-600"/></button></td>
+            <td>
+                <button onClick={handleRemove}>
+                    <AiFillDelete className="text-2xl ease-linear transition-colors hover:text-red-600" />
+                </button>
+            </td>
         </tr>
     );
 }
 
 export default function ToDoList() {
-
-    const [totalEntries, setTotalEntries] = useState(0);
-    const [entries, setEntries] = useState(JSON.parse(localStorage.getItem("todolist")));
-    const [entryField, setEntryField] = useState('');
+    const [entries, setEntries] = useState(
+        JSON.parse(localStorage.getItem("todolist"))
+    );
+    const [entryField, setEntryField] = useState("");
     const rows = [];
-
 
     function addEntry() {
         if (entryField === "") return;
         const nextEntries = entries.slice();
-        nextEntries.push({ key: totalEntries, text: entryField, done: false });
-        setTotalEntries(totalEntries + 1);
+        nextEntries.push({
+            key: Date.now().toString(),
+            text: entryField,
+            done: false,
+        });
         setEntries(nextEntries);
         setEntryField("");
         saveEntries(nextEntries);
@@ -63,45 +75,55 @@ export default function ToDoList() {
         saveEntries(nextEntries);
     }
 
-    function saveEntries(entries_to_save){
+    function saveEntries(entries_to_save) {
         localStorage.setItem("todolist", JSON.stringify(entries_to_save));
     }
 
     entries.forEach((entry) => {
         rows.push(
             <TDEntry
-                key={entry.key} text={entry.text} checked={entry.done}
+                key={entry.key}
+                text={entry.text}
+                checked={entry.done}
                 handleCheck={() => checkEntry(entry.key)}
                 handleRemove={() => deleteEntry(entry.key)}
             />
-        )
-    })
+        );
+    });
 
     return (
-        <div className="flex flex-col gap-2 p-5 bg-primary border border-white shadow-lg shadow-highlight_darker 
-        min-h-[38rem] min-w-[30rem] justify-start">
+        <div
+            className="flex flex-col gap-2 p-5 bg-primary border border-white shadow-lg shadow-highlight_darker 
+        min-h-[38rem] min-w-[30rem] justify-start"
+        >
             <p className="basis-2 text-3xl p-3 text-center">TO DO LIST</p>
             <div className="">
-                <form method="post"
+                <form
+                    method="post"
                     className="flex p-2 gap-2"
-                    onSubmit={e => {
+                    onSubmit={(e) => {
                         e.preventDefault();
                         addEntry();
-                    }}>
+                    }}
+                >
                     <input
                         type="text"
                         value={entryField}
                         placeholder="type here"
-                        onChange={e => setEntryField(e.target.value)}
-                        className="grow text-xl bg-primary hover:border-highlight_darker hover:text-blue-400 my_text_anim border p-2 " />
-                    <button type="submit" className="text-xl my_text_anim my_button p-1">Add</button>
+                        onChange={(e) => setEntryField(e.target.value)}
+                        className="grow text-xl bg-primary hover:border-highlight_darker hover:text-blue-400 my_text_anim border p-2 "
+                    />
+                    <button
+                        type="submit"
+                        className="text-xl my_text_anim my_button p-1"
+                    >
+                        Add
+                    </button>
                 </form>
             </div>
             <table className="table-auto border-collapse min-w-[20rem]">
-                <tbody className="">
-                    {rows}
-                </tbody>
+                <tbody className="">{rows}</tbody>
             </table>
         </div>
-    )
+    );
 }
